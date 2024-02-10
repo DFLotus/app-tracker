b53c6a4b9baf6155db6a8d59836140171807cb3c
DFLotus <deondraefrancis101@gmail.com>
2024-02-10 16:29:37 -0500

    Added data base connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,16 @@
 import os
 from flask import Flask
+import psycopg2
+import sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+from db_Config import connect_to_db
 
 
 # To run the application flask --app app  run --debug
-def create_app(test_config=None):
+def create_app(test_config=None) -> Flask:
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    """
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-    """
+    app: Flask = Flask(__name__, instance_relative_config=True)
+
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile("config.py", silent=True)
@@ -24,4 +23,13 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    app.db_conn = connect_to_db()
+
+    # When connection is closed the application is terminated
+    @app.teardown_appcontext
+    def teardown_db(exception=None):
+        if hasattr(app, "db_conn"):
+            app.db_conn.close()
+
     return app
